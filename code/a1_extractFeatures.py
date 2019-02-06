2f031709430c38007b93424e91670ed5bb2df158
ooo125258 <michaelsundmlis@gmail.com>
2019-02-06 17:30:12 -0500

    features 14

--- code/a1_extractFeatures.py
+++ code/a1_extractFeatures.py
@@ -6,9 +6,22 @@ import json
 from collections import defaultdict
 from numpy import loadtxt
 
-def init():
-
+def wordTagSplit(token):
+    format = re.match(r'^(.*)\/(\w+)$', token, re.I)
+    if len(format) == 2:
+        (word, tag) = format
+    else:
+        print("something strange input! word: {}".format(token))
+        return None
+    return word, tag
+        
+def helper_8(token):
+    format = re.match(r'^(.*)\/(\w+)$', token, re.I)
+        
 def extract1( comment ):
+    #TODO:how do you deal with ?!?
+    #TODO: if only one char, it's really possible to get error!
+    #TODO: handle xx
     ''' This function extracts features from a single comment
 
     Parameters:
@@ -20,20 +33,90 @@ def extract1( comment ):
     print('TODO')
     # TODO: your code here
     PRP_1st = loadtxt("/u/cs401/Wordlists/First-person", comments="#", delimiter="\n", unpack=False)
-    PRP_1st = loadtxt("/u/cs401/Wordlists/Second-person", comments="#", delimiter="\n", unpack=False)
-    PRP_1st = loadtxt("/u/cs401/Wordlists/Third-person", comments="#", delimiter="\n", unpack=False)
+    PRP_2nd = loadtxt("/u/cs401/Wordlists/Second-person", comments="#", delimiter="\n", unpack=False)
+    PRP_3rd = loadtxt("/u/cs401/Wordlists/Third-person", comments="#", delimiter="\n", unpack=False)
+    future_tense_verbs = ["'ll", "will", "gonna"] #The last one is going to VB, consider later
+    slang = loadtxt("/u/cs401/Wordlists/Slang", comments="#", delimiter="\n", unpack=False)
     #Now, start to check the words
     sentences = comment.split("\n")
 
-    numbers = {}
+    numbers = [0] * 30 #This is for first 29.
     for sentence in sentences:
         #to avoid some silly empty string.
         if sentence == "" or sentence is None:
             continue
 
-        words = sentence.split()
-        # 1, first person pronoun
-        for i in range(len(words)):
+        tokens = sentence.split()
+
+        for i in range(len(tokens)):
+            if tokens[i] = "":
+                continue
+            #The word should be word/tag format
+            ret = wordTagSplit(tokens[i])
+            word = ""
+            tag = ""
+            if ret is None:
+                print("something strange input! sentence: {} word: {}".format(sentence, tokens[i]))
+                continue
+            else:
+                (word, tag) = ret
+            
+            # 1,2,3, 1st/2nd/3rd person pronoun
+            lower_word = word.lower()
+            if lower_word in PRP_1st:
+                numbers[1] += 1
+            elif lower_word in PRP_2nd:
+                numbers[2] += 1
+            elif lower_word in PRP_3rd:
+                numbers[3] += 1
+            #4, CC
+            elif tag == "CC":
+                numbers[4] += 1
+            #5, VBD
+            elif tag == "VBD":
+                numbers[5] += 1
+            #6, Future
+            #going to will be handled in present TODO: related to "will"
+            elif lower_word in future_tense_verbs:
+                numbers[6] += 1
+            elif lower_word == "going" and i <= len(tokens) - 2:
+                #judge for to do
+                #pos_for_TO: 
+                first_token = wordTagSplit(tokens[i + 1])                
+                second_token = wordTagSplit(tokens[i + 2])
+                if first_token[0].lower() == "to" and first_token[1] in ["VB", "VBG", "VBP"]:
+                    numbers[6] += 1
+            #7, comma
+            elif lower_word == ',':
+                numbers[7] += 1
+            #8, multi-char punctuation tokens
+            #multi
+            elif len(lower_word) > 1 and all(i in string.punctuation for i in lower_word):
+                numbers[8] += 1
+            #9, common nouns
+            elif tag in ["NN", "NNS"]:
+                numbers[9] += 1
+            #10, proper nouns
+            elif tag in ["NNP", "NNPS"]:
+                numbers[10] += 1
+            #11, adberbs
+            elif tag in ["RB", "RBR", "RBS", "RP"]:
+                numbers[11] += 1
+            #12, wh- words
+            elif tag in ["WDT", "WP", "WP$", "WRB"]:
+                numbers[12] += 1
+            #13, slang acronyms
+            elif tag in slang:
+                numbers[13] += 1
+            #14, words in uppercase
+            elif len(word) >= 3 and word.isupper():
+                numbers[14] += 1
+            else:
+                if tag == "xx":
+                    print("unknown xx word: {} in sentence {}.".format(tokens[i], sentence))
+                else:
+                    print("unknown other word: {} in sentence {}.".format(tokens[i], sentence))
+
 
 
 def main( args ):
