b13fdc112208429a1ae4cd6103cca66b8383bf52
ooo125258 <scq00000>
2019-03-28 21:05:51 -0400

    before test. All code done

--- a3/a3_gmm.py
+++ a3/a3_gmm.py
@@ -30,22 +30,20 @@ def log_b_m_x(m, x, myTheta, preComputedForM=[]):
     inv_sigmaSqr[mask] = np.reciprocal(myTheta.Sigma[m][mask])
     square_x = np.square(x).T    
     term1_1 = 0.5 * np.dot(inv_sigmaSqr, square_x)
-    term1_2 = myTheta.mu[m] * np.dot(inv_sigmaSqr, square_x)
+    term1_2 = np.sum(myTheta.mu[m] * inv_sigmaSqr * x)
     term1_beforesum = term1_1 - term1_2
     term1 = np.sum(term1_beforesum)# TODO: axis of term1?
 
     term2 = 0
-    # checker:
-    if len(preComputedForM) == 0:
-        mask = myTheta.Sigma[m] != 0
-        inv_sigmaSqr = np.array(myTheta.Sigma[m], copy=True)
-        inv_sigmaSqr[mask] = np.reciprocal(myTheta.Sigma[m][mask])
+    # checker:# Assume, precomputedform is a m len list.
+    if len(preComputedForM) != myTheta.Sigma.shape[0] \
+            or len(np.array(preComputedForM).flatten()) != myTheta.Sigma.shape[0]:
         
         term2_1_inner = 0.5 * np.square(myTheta.mu[m]) * inv_sigmaSqr
-        term2_1 = np.sum(term2_1_inner, axis=1)
+        term2_1 = np.sum(term2_1_inner)
         term2_2 = d / 2 * np.log(2 * np.pi)
         term2_3_inner = 0.5 * np.log(myTheta.Sigma[m], where=(myTheta.Sigma[m] != 0.))
-        term2_3 = np.sum(term2_3_inner, axis = 1)# TODO: check when sigma is not 1, if it still performs correctly.
+        term2_3 = np.sum(term2_3_inner)# TODO: check when sigma is not 1, if it still performs correctly.
         term2 = term2_1 + term2_2 + term2_3
     else:
         term2 = preComputedForM[m]
@@ -54,31 +52,59 @@ def log_b_m_x(m, x, myTheta, preComputedForM=[]):
     return rst
     print ( 'TODO' )
 
-def log_b_m_x_given(x, myTheta, preComputedForM=[], term2_1=[]):
+def log_b_m_x_given(x, myTheta, preComputedForM=[]):
     ''' Returns the log probability of d-dimensional vector x using only component m of model myTheta
         See equation 1 of the handoutd
 
         As you'll see in tutorial, for efficiency, you can precompute something for 'm' that applies to all x outside of this function.
         If you do this, you pass that precomputed component in preComputedForM
     '''
-    #Format pt.2. Term2 might be preComputed
+    #In vector form, precompute part would be slower. As more steps need to be made.
     #TODO: need to debug when the train is permitted. At least the np dots.
+    M, d = myTheta.Sigma.shape
+    T = x.shape[0]
+    # log_Bs = np.zeros((M, T))
+    # for m in range(M):
+    #     term1 = np.sum(np.square(X - myTheta.mu[m]) / (2 * myTheta.Sigma[m]), axis=1)
+    #     term2 = d / 2 * np.log(2 * np.pi)
+    #     term3 = 1. / 2 * np.sum(np.log(np.square(np.prod(myTheta.Sigma[m]))))
+    #     log_bs = - term1 - term2 - term3
+    #     log_Bs[m] = log_bs
+    # return log_Bs
+    #
+
     mask = myTheta.Sigma != 0
     inv_sigmaSqr = np.array(myTheta.Sigma, copy=True)
     inv_sigmaSqr[mask] = np.reciprocal(myTheta.Sigma[mask])
-
-    term1_1_step = np.einsum("ik,jk->ijk", inv_sigmaSqr, np.square(x))
-    term1_1 = 0.5 * term1_1_step
-    term1_2_step = np.einsum("ik,jk->ijk", inv_sigmaSqr, x)
-
-    term1_2 = np.einsum("ik,ijk->ijk", myTheta.mu, term1_2_step)
-    term1_beforesum = term1_1 - term1_2
-    term1 = np.sum(term1_beforesum, axis=-1)# TODO: axis of term1?
-    T =x.shape[0]
-    preComputedForM_expanded = np.repeat(preComputedForM, T).reshape(-1, T)
-    term2 = preComputedForM_expanded
+    term1_wosum = np.zeros((M,T))
+    for m in range(M):
+        term1_m = 0.5 * np.square(x - myTheta.mu[m]) #T * d - d = T*d
+        term1_wosum[m] = np.dot(term1_m, inv_sigmaSqr[m]) #T*d, d = T
+    term1 = term1_wosum #M * T, M
+    term2 = d / 2 * np.log(2 * np.pi)
+    term3_inner = 0.5 * np.log(myTheta.Sigma, where=(myTheta.Sigma != 0.))
+    term3 = np.sum(term3_inner, axis=1)  # TODO: check when sigma is not 1, if it still performs correctly.
+    rst = -((term1.T + term3.T).T + term2)
     
-    rst = - term1 - term2
+    
+    # mask = myTheta.Sigma != 0
+    # inv_sigmaSqr = np.array(myTheta.Sigma, copy=True)
+    # inv_sigmaSqr[mask] = np.reciprocal(myTheta.Sigma[mask])
+    #
+    # #term1_1 = 0.5 * np.dot(inv_sigmaSqr, np.square(x).T)
+    # term1_1_step = np.einsum("ik,jk->ijk", inv_sigmaSqr, np.square(x))
+    # term1_1 = 0.5 * term1_1_step
+    # term1_2_step = np.einsum("ik,jk->ijk", inv_sigmaSqr, x)
+    # ##term1_2 = np.dot()
+    #
+    # term1_2 = np.einsum("ik,ijk->ijk", myTheta.mu, term1_2_step)
+    # term1_beforesum = term1_1 - term1_2
+    # term1 = np.sum(term1_beforesum, axis=-1)#TODO: axis of term1?
+    # T =x.shape[0]
+    # preComputedForM_expanded = np.repeat(preComputedForM, T).reshape(-1, T)
+    # term2 = preComputedForM_expanded
+    #
+    # rst = - term1 - term2
     return rst
     print ( 'TODO' )
 
@@ -99,7 +125,7 @@ def log_p_m_x( m, x, myTheta):
     return rst
     print ( 'TODO' )
 
-def log_p_m_x_given( m, log_Bs, t, myTheta, preComputedForM = []):
+def log_p_m_x_given(  m, log_Bs, t, myTheta, preComputedForM = []):
     ''' Returns the log probability of the m^{th} component given d-dimensional vector x, and model myTheta
         Given Bs
     '''
@@ -154,7 +180,8 @@ def train( speaker, X, M=8, epsilon=0.0, maxIter=20 ):
     T = X.shape[0]
     M, d = myTheta.Sigma.shape
     myTheta.Sigma[:,:] = 1
-    indices = np.random.choice(X.shape[0], M, replace=False)
+    #indices = np.random.choice(X.shape[0], M, replace=False)
+    indices = np.array(random.sample(range(T), M))
     myTheta.mu = X[indices]
     myTheta.omega[:, 0] = 1. / M
     
@@ -168,6 +195,8 @@ def train( speaker, X, M=8, epsilon=0.0, maxIter=20 ):
     #while i =< maxIter and improvement >=  do
     while i <= maxIter and improvement >= epsilon:
         precomputed = preComputedForEachM(myTheta)
+        log_b_m_x(0, X[0], myTheta, precomputed) #TODO: wrong
+        
         log_Bs = log_b_m_x_given(X, myTheta, precomputed)
         #Bs = np.exp(log_Bs)
         
@@ -220,7 +249,7 @@ def test( mfcc, correctID, models, k=5 ):
         #for m in range(M):
             #for t in range(T):
                 #log_Bs[m, t] = log_b_m_x(m, mfcc[t], models[i], precomputed)
-        log_Bs = log_b_m_x_given(mfcc, models[i], precomputed)
+        log_Bs = log_b_m_x_given(mfcc, models[i])
 
         Ls[i] = logLik(log_Bs, models[i])
     if k > 0:
@@ -233,9 +262,10 @@ def test( mfcc, correctID, models, k=5 ):
         for i in range(len(best_k)):
             print("Model: {} Likelihood: {}".format(models[best_k[i]].name, Ls[best_k[i]]))
         '''
-        print("\n" + models[bestModel].name)
+        print("\n" + models[correctID].name)
         for i in range(len(best_k)):
             print(models[best_k[i]].name + " " + str(Ls[best_k[i]]))
+        print("bestModel: {} correctID: {}".format(bestModel, correctID))
     return 1 if (bestModel == correctID) else 0
 
 
@@ -268,7 +298,8 @@ def UpdateParameters(myTheta, X, Ps, L):
     #Ps = np.exp(log_Ps)
     newTheta.omega = np.average(Ps, axis=1).reshape(-1,1)
     #Weighted mean, weight xt, mean p
-    newTheta.mu = np.divide(np.dot(Ps, X), np.sum(Ps, axis=1).reshape(-1,1))
+    
+    newTheta.mu = np.divide(np.dot(Ps, X), np.sum(Ps, axis=1).reshape(-1,1))# Todo: Wait, ps might be zero???
 
     square_x_term = np.divide(np.dot(Ps, np.square(X)), np.sum(Ps, axis=1).reshape(-1,1))
     newTheta.Sigma = square_x_term - np.square(newTheta.mu)
@@ -279,7 +310,7 @@ if __name__ == "__main__":
     load = False
     trainThetas = []
     testMFCCs = []
-    print('TODO: you will need to modify this main block for Sec 2.3')
+    #print('TODO: you will need to modify this main block for Sec 2.3')
     d = 13
     k = 5  # number of top speakers to display, <= 0 if none
     M = 8
@@ -294,8 +325,7 @@ if __name__ == "__main__":
     
                 files = fnmatch.filter(os.listdir( os.path.join( dataDir, speaker ) ), '*npy')
                 random.shuffle( files )
-                
-                testMFCC = np.load( os.path.join( dataDir, speaker, files.pop() ) )
+                testMFCC = np.load( os.path.join( dataDir, speaker, files.pop(0) ) )
                 testMFCCs.append( testMFCC )
     
                 X = np.empty((0,d))
--- a3/test.py
+++ a3/test.py
@@ -5,6 +5,55 @@ import pylab
 import os
 import fnmatch
 
+
+def execute(M, maxIter, maxSpeaker, f):
+    trainThetas = []
+    testMFCCs = []
+    
+    d = 13
+    k = 5  # number of top speakers to display, <= 0 if none
+    epsilon = 0.0
+    
+    trainThetas = []
+    testMFCCs = []
+    for subdir, dirs, files in os.walk(dataDir):
+        for speaker in tqdm(dirs):
+            print("Speaker: {}, Iteration: {} M:{}")
+            print(speaker, file=f)
+            
+            files = fnmatch.filter(os.listdir(os.path.join(dataDir, speaker)), '*npy')
+            random.shuffle(files)
+            
+            if s < maxSpeaker:
+                testMFCC = np.load(os.path.join(dataDir, speaker, files.pop()))
+                testMFCCs.append(testMFCC)
+                
+                # if s < maxSpeaker:
+                X = np.empty((0, d))
+                for file in files:
+                    myMFCC = np.load(os.path.join(dataDir, speaker, file))
+                    X = np.append(X, myMFCC, axis=0)
+                
+                trainThetas.append(train(speaker, X, M, epsilon, maxIter))
+            else:
+                trainThetas.append(theta(speaker))
+            s += 1
+    
+    # evaluate
+    numCorrect = 0
+    for i in range(0, len(testMFCCs)):
+        numCorrect += test(testMFCCs[i], i, trainThetas, k)
+    
+    accuracy = 1.0 * numCorrect / len(testMFCCs)
+    print('Accuracy: ', accuracy, file=f)
+    print('M: {} \t maxIter: {} \t S: {} \t Accuracy: {}'.format(M, maxIter, maxSpeaker, accuracy))
+    print('\n', file=f)
+    
+    # write to file
+    print('M: {} \t maxIter: {} \t S: {} \t Accuracy: {}'.format(M, maxIter, maxSpeaker, accuracy))
+
+
+
 if __name__ == "__main__":
 
     random.seed(0)
@@ -21,47 +70,16 @@ if __name__ == "__main__":
     # maxSpeaker = 32
     f = open('gmm_S.txt', 'w')
 
-    M_list = [8, 7, 6, 5, 4, 3, 2, 1]
-    maxIter_list = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0]
-    maxSpeaker_list = [32, 24, 16, 8, 4]
+    M_list = [20, 15, 10, 8, 5, 2, 1][::-1]
+    maxIter_list = [50, 30, 20, 15, 10, 5, 2, 1][::-1]
+    maxSpeaker_list = [32, 20, 15, 10, 5, 2, 1][::-1]
 
-    # for M in M_list:
-    # for maxIter in maxIter_list:
+    for M in tqdm(M_list):
+        execute(M, 20, 100)
+    for maxIter in tqdm(maxIter_list):
+        execute(8, maxIter, 100)
     for maxSpeaker in tqdm(maxSpeaker_list):
-        s = 0
-        trainThetas = []
-        testMFCCs = []
-        for subdir, dirs, files in os.walk(dataDir):
-            for speaker in tqdm(dirs):
-                print(speaker, file=f)
+        execute(8, 20, maxSpeaker)
+    
+    f.close()
 
-                files = fnmatch.filter(os.listdir(os.path.join(dataDir, speaker)), '*npy')
-                random.shuffle(files)
-
-                if s < maxSpeaker:
-                    testMFCC = np.load(os.path.join(dataDir, speaker, files.pop()))
-                    testMFCCs.append(testMFCC)
-
-                # if s < maxSpeaker:
-                    X = np.empty((0, d))
-                    for file in files:
-                        myMFCC = np.load(os.path.join(dataDir, speaker, file))
-                        X = np.append(X, myMFCC, axis=0)
-
-                    trainThetas.append(train(speaker, X, M, epsilon, maxIter))
-                else:
-                    trainThetas.append(theta(speaker))
-                s += 1
-
-        # evaluate
-        numCorrect = 0
-        for i in range(0, len(testMFCCs)):
-            numCorrect += test(testMFCCs[i], i, trainThetas, k)
-
-        accuracy = 1.0 * numCorrect / len(testMFCCs)
-        print('Accuracy: ', accuracy, file=f)
-        print('M: {} \t maxIter: {} \t S: {} \t Accuracy: {}'.format(M, maxIter, maxSpeaker, accuracy), file=f)
-        print('\n', file=f)
-
-        # write to file
-        print('M: {} \t maxIter: {} \t S: {} \t Accuracy: {}'.format(M, maxIter, maxSpeaker, accuracy), file=f)
